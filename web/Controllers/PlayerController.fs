f0fc763822870582bf69e1ec4313e965093d4437
ProgramFOX <programfox@hotmail.be>
2017-06-12 22:24:41 +0200

    Controller: make GetPlayer return the distribution

--- web/Controllers/PlayerController.fs
+++ web/Controllers/PlayerController.fs
@@ -3,15 +3,22 @@ namespace AntichessCheatDetection.Controllers
 open Microsoft.AspNetCore.Mvc
 
 open AntichessCheatDetection.Modules.Reference
+open AntichessCheatDetection.Modules.Player
 
 type PlayerController(referenceDbRepo: IReferenceDbRepo) =
     inherit Controller()
 
     [<Route("/Player/{lookup}/{name}")>]
-    member this.GetPlayer(lookup : string, name : string) =
+    member this.GetPlayer(lookup : string, name : string, speed : string, minRating : int, maxRating: int, engine : string, step : int) =
         let lowerLookup = lookup.ToLower()
         let lowerName = name.ToLower()
+        let stepF = float step
         match lowerLookup with
-        | "reference" -> this.Content(referenceDbRepo.GetGamesByPlayer(lowerName).Count.ToString())
+        | "reference" ->
+            referenceDbRepo.GetGamesByPlayer(lowerName) |>
+            PlayerInfoProcessor.getPlayerDistribution speed minRating maxRating engine stepF |>
+            Seq.map string |>
+            String.concat "," |>
+            this.Content
         | "investiage" -> this.Content("not yet implemented")
         | _ -> this.Content("Invalid lookup type.")

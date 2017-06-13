3b3f19db6b90dcf32f21fb36c5a03657584e1c97
ProgramFOX <programfox@hotmail.be>
2017-06-13 16:31:05 +0200

    GetPlayer: return all game info (so distribution will be calculated client-side)

--- web/Controllers/PlayerController.fs
+++ web/Controllers/PlayerController.fs
@@ -9,16 +9,15 @@ type PlayerController(referenceDbRepo: IReferenceDbRepo) =
     inherit Controller()
 
     [<Route("/Player/{lookup}/{name}")>]
-    member this.GetPlayer(lookup : string, name : string, speed : string, minRating : int, maxRating: int, engine : string, step : int) =
+    member this.GetPlayer(lookup : string, name : string, speed : string, minRating : int, maxRating: int, engine : string, step : int) : IActionResult =
         let lowerLookup = lookup.ToLower()
         let lowerName = name.ToLower()
         let stepF = float step
         match lowerLookup with
         | "reference" ->
-            referenceDbRepo.GetGamesByPlayer(lowerName) |>
-            PlayerInfoProcessor.getPlayerDistribution speed minRating maxRating engine stepF |>
-            Seq.map string |>
-            String.concat "," |>
-            this.Content
-        | "investigate" -> this.Content("not yet implemented")
-        | _ -> this.Content("Invalid lookup type.")
+            referenceDbRepo.GetGamesByPlayer lowerName |>
+            PlayerInfoProcessor.filterSpeed speed |>
+            PlayerInfoProcessor.filterRating minRating maxRating |>
+            this.Json :> IActionResult
+        | "investigate" -> this.Content("not yet implemented") :> IActionResult
+        | _ -> this.Content("Invalid lookup type.") :> IActionResult

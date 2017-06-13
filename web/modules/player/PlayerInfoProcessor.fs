f9734cd3a7046209b9fcf971cd5e4b6c8778032f
ProgramFOX <programfox@hotmail.be>
2017-06-13 16:32:26 +0200

    Remove unused PlayerInfoProcessor functions

--- web/modules/player/PlayerInfoProcessor.fs
+++ web/modules/player/PlayerInfoProcessor.fs
@@ -12,31 +12,3 @@ module PlayerInfoProcessor =
 
     let filterRating minR maxR (gameList : seq<Reference>) =
         Seq.filter (fun (x : Reference) -> minR <= x.Rating && maxR >= x.Rating) gameList
-    
-    let takeEngineValues engine gameList =
-        match engine with
-        | "sjeng" -> Seq.map (fun (x : Reference) -> x.Sjeng) gameList
-        | "stockfish" -> Seq.map (fun (x : Reference) -> x.Stockfish) gameList
-        | "max" -> Seq.map (fun (x : Reference) -> x.Max) gameList
-        | _ -> raise(System.Exception())
-    
-    let roundToClosestStepMultiple step engineValues = 
-        Seq.map (fun x -> floor(x * 100.0 / step) * step) engineValues
-    
-    let countByStep engineValues =
-        Seq.countBy id engineValues
-    
-    let sortCountsByStep counts =
-        Seq.sortBy fst counts
-    
-    let onlyGetCountValues counts =
-        Seq.map snd counts
-    
-    let getPlayerDistribution speed minRating maxRating engine step gameList =
-        gameList |> filterSpeed speed
-                 |> filterRating minRating maxRating
-                 |> takeEngineValues engine
-                 |> roundToClosestStepMultiple step
-                 |> countByStep
-                 |> sortCountsByStep
-                 |> onlyGetCountValues

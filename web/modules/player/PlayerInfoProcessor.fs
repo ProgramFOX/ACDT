3788da2ce1bd36b71e8f5cdfcada5444f4695688
ProgramFOX <programfox@hotmail.be>
2017-06-16 19:45:48 +0200

    PlayerInfoProcessor: accept both Reference and Investigation

--- web/modules/player/PlayerInfoProcessor.fs
+++ web/modules/player/PlayerInfoProcessor.fs
@@ -1,14 +1,26 @@
 namespace AntichessCheatDetection.Modules.Player
 
 open AntichessCheatDetection.Modules.Reference
+open AntichessCheatDetection.Modules.Investigate
 
 open Microsoft.FSharp.Collections
 
 module PlayerInfoProcessor =
-    let filterSpeed speed (gameList : seq<Reference>) =
+    let getSpeedFromGame (game : obj) =
+        match game with
+        | :? Reference as r -> r.Speed
+        | :? Investigation as i -> i.Speed
+        | _ -> raise(System.Exception())
+    
+    let getRatingFromGame (game : obj) =
+        match game with
+        | :? Reference as r -> r.Rating
+        | :? Investigation as i -> i.Rating
+        | _ -> raise(System.Exception())
+    let filterSpeed<'T> speed (gameList : seq<'T>) =
         match speed with
         | "all" -> gameList
-        | _ -> Seq.filter (fun x -> x.Speed = speed) gameList
+        | _ -> Seq.filter (fun x -> getSpeedFromGame x = speed) gameList
 
-    let filterRating minR maxR (gameList : seq<Reference>) =
-        Seq.filter (fun (x : Reference) -> minR <= x.Rating && maxR >= x.Rating) gameList
+    let filterRating<'T> minR maxR (gameList : seq<'T>) =
+        Seq.filter (fun x -> minR <= getRatingFromGame x && maxR >= getRatingFromGame x) gameList

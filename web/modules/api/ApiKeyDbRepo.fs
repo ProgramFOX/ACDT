ca48bb8bc2a7188f88680a621063a441b8685ee5
ProgramFOX <programfox@hotmail.be>
2017-06-22 20:10:38 +0200

    Implement ApiKeyDbRepo

--- web/modules/api/ApiKeyDbRepo.fs
+++ web/modules/api/ApiKeyDbRepo.fs
@@ -1,4 +1,26 @@
 namespace AntichessCheatDetection.Modules.Api
 
+open AntichessCheatDetection.Modules.Configuration
+
+open MongoDB.Driver
+open MongoDB.Bson
+open Microsoft.Extensions.Options
+open System
+open System.Collections.Generic
+open System.Security.Cryptography
+open System.Text
+
 type IApiKeyDbRepo =
-    abstract member IsValidKey: bool 
+    abstract member IsValidKey: string -> bool
+
+type ApiKeyDbRepo(settings: IOptions<Settings>) =
+    let extractedSettings = settings.Value
+    let collection = MongoClient(extractedSettings.MongoConnectionString : string).GetDatabase(extractedSettings.Database).GetCollection<BsonDocument>(extractedSettings.ApiKeyCollectionName)
+
+    interface IApiKeyDbRepo with
+        member this.IsValidKey unhashedKey =
+            let bytes = Encoding.ASCII.GetBytes unhashedKey
+            let sha1 = SHA1.Create()
+            let hash = sha1.ComputeHash bytes
+            let hashHex = BitConverter.ToString(hash).Replace("-", "").ToLower()
+            collection.Find(Builders<BsonDocument>.Filter.Eq(StringFieldDefinition<BsonDocument, string>("hash"), hashHex)).Any()

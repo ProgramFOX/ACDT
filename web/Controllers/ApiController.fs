36c30d243045e3cce46e4e92b2defa49bf020437
ProgramFOX <programfox@hotmail.be>
2017-06-25 14:17:28 +0200

    Fix API key verification

--- web/Controllers/ApiController.fs
+++ web/Controllers/ApiController.fs
@@ -12,8 +12,10 @@ type ApiController(apiKeyDbRepo: IApiKeyDbRepo, queueDbRepo: IQueueDbRepo) =
     override this.OnActionExecuting (context : ActionExecutingContext) =
         match context.HttpContext.Request.Method.ToUpperInvariant() with
         | "POST" ->
-            let ok, apiKeyO = context.RouteData.Values.TryGetValue("key")
+            let ok, apiKeyO = context.ActionArguments.TryGetValue("key")
             let apiKey = string apiKeyO
+            System.Console.WriteLine(ok)
+            System.Console.WriteLine(apiKey)
             match ok && apiKeyDbRepo.IsValidKey(apiKey) with
             | true ->
                 base.OnActionExecuting(context)
@@ -23,6 +25,6 @@ type ApiController(apiKeyDbRepo: IApiKeyDbRepo, queueDbRepo: IQueueDbRepo) =
             context.Result <- this.BadRequest() :> IActionResult
     
     [<Route("/Api/QueueNext")>]
-    member this.Queue() =
+    member this.Queue(key: string) =
         let nextQueueItem = queueDbRepo.GetNextQueueItem()
         this.Content(if isNull nextQueueItem then "" else nextQueueItem.Name)

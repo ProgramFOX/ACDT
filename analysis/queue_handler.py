a792e42f3fc47dc83febcffbd4a8f97ee088729f
ProgramFOX <programfox@hotmail.be>
2017-07-01 20:46:14 +0200

    [WIP] Construct for loop of investigate_one_player

--- analysis/queue_handler.py
+++ analysis/queue_handler.py
@@ -13,18 +13,23 @@ def load_api_key():
     with open("analysis/secret/apikey.txt", "r") as file_object:
         return file_object.read().strip()
 
-def investigate_one_player(user_id, session):
+def investigate_one_player(user_id, session, stockfish, sf_info_handler):
     """Investigates one player and pushes the result to the server."""
     download_status, downloaded_games = downloader.download_one(user_id, session)
     if download_status == downloader.DownloadStatus.success:
         games = json.loads(downloaded_games)
         for game in games:
-            engine_check.analyze_one_game ...
+            moves = game["turns"].split(" ")
+            analyze_white = game["players"]["white"]["userId"] == user_id
+
+            engine_check.analyze_one_game(moves, analyze_white, stockfish, sf_info_handler)
 
 def main():
     """Main method of script: where the 'real work' happens."""
     session = requests.Session()
     api_key = load_api_key()
+
+    stockfish, sf_info_handler = engine_check.create_stockfish_instance()
 
 if __name__ == "__main__":
     main()
